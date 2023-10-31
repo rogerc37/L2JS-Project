d60efc5b816d12d0ac15671ace52337f56d00fb7
JingNL <145472716+JingNL@users.noreply.github.com>
2023-10-31 11:03:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var elementClicked = false;
+var element = document.getElementsByClassName('grid');
+element.addEventListener('click', onClick);
+function initialize()
+{
+    const clicks = 0;
+    const counter = 0;
+}
+function onClick()
+{
+    elementClicked = true;
+}
